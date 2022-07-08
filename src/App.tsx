25db2a35905b80afead88b7846aacfe36a271d40
EricWu <skynocover@gmail.com>
2022-07-08 22:49:01 +0800

    chore: 整理import

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
-import { amber, deepOrange, grey } from '@mui/material/colors';
-import { PaletteMode } from '@mui/material';
-// import {grey} from '@mui/material/colors'
 
 import Home from './pages/Home';
 import About from './pages/About';
